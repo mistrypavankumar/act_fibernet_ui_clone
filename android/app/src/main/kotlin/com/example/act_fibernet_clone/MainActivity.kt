50f9958c71f541831d1e365cae7974d93f62aa92
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-08-31 01:08:47 +0530

    Added files

---
+++ android/app/src/main/kotlin/com/example/act_fibernet_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.act_fibernet_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(),
+        drawer: Drawer(
+          child: Column(
+            children: [
+              CircleAvatar(
+                child: Icon(Icons.people_alt_outlined),
+              ),
+              Text("UserName"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

e7139a83efb0115dd5cd6e641a628087f59b39d8
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-09-27 02:30:42 +0530

    Added TabViews

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,6 @@
+import 'package:act_fibernet_clone/screens/tabView/accountView.dart';
+import 'package:act_fibernet_clone/screens/tabView/homeTabView.dart';
+import 'package:act_fibernet_clone/screens/tabView/socialTabView.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,164 +12,189 @@ class _HomeScreenState extends State<HomeScreen> {
   bool isClicked = true;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Hi John"),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.notifications),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      bottom: 10,
-                      left: 5,
-                      child: Column(
-                        children: [
-                          Container(
-                            height: 40,
-                            width: 40,
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.white,
-                                width: 2,
-                              ),
-                              borderRadius: BorderRadius.circular(50),
-                            ),
-                            child: Icon(
-                              Icons.perm_identity,
-                              color: Colors.white,
-                              size: 35.0,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            "John",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+    return DefaultTabController(
+      length: 3,
+      child: Container(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Hi John"),
+            elevation: 0.0,
+            actions: [
+              IconButton(
+                icon: Icon(Icons.notifications),
+                onPressed: () {},
               ),
-              buildListTile("My Account", Icons.people, () {}),
-              buildListTile("My Wi-Fi Settings", Icons.wifi, () {}),
-              buildListTile("Upgrade my plan", Icons.upgrade, () {}),
-              buildListTile("Shop", Icons.shopping_basket, () {}),
-              buildListTile("Offers", Icons.local_offer, () {}),
-              buildListTile(
-                  "Notifications", Icons.notification_important, () {}),
-              buildListTile("FAQ's", Icons.perm_identity, () {}),
-              buildListTile("About us", Icons.people_alt, () {}),
-              buildListTile("Rate This App", Icons.star, () {}),
-              buildListTile("Logout", Icons.logout, () {}),
-              buildListTile("App version: 0.0.1", Icons.new_releases, () {}),
             ],
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.white,
-          child: Container(
-            height: 70.0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  children: [
-                    IconButton(
-                      icon: Icon(
-                        Icons.home_outlined,
-                        size: 30.0,
-                        color: Colors.red,
-                      ),
-                      onPressed: () {},
-                    ),
-                    Text(
-                      "Home",
-                      style: TextStyle(
-                        fontSize: 10,
-                        color: Colors.red,
-                      ),
-                    ),
-                  ],
+            bottom: TabBar(
+              indicatorWeight: 5,
+              tabs: [
+                Tab(
+                  text: "Account",
                 ),
-                Column(
-                  children: [
-                    IconButton(
-                      icon: Icon(
-                        Icons.support_agent,
-                        size: 30.0,
-                        color: Colors.grey,
-                      ),
-                      onPressed: () {},
-                    ),
-                    Text(
-                      "Support",
-                      style: TextStyle(
-                        fontSize: 10,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
+                Tab(
+                  text: "Home",
                 ),
-                Column(
-                  children: [
-                    IconButton(
-                      icon: Icon(
-                        Icons.east_rounded,
-                        size: 30.0,
-                        color: Colors.grey,
-                      ),
-                      onPressed: () {},
-                    ),
-                    Text(
-                      "Refer & Earn",
-                      style: TextStyle(
-                        fontSize: 10,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    IconButton(
-                      icon: Icon(
-                        Icons.list,
-                        size: 30.0,
-                        color: isClicked ? Colors.red : Colors.grey[600],
-                      ),
-                      onPressed: () {},
-                    ),
-                    Text(
-                      "Bill details",
-                      style: TextStyle(
-                        fontSize: 10,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
+                Tab(
+                  text: "Social",
                 ),
               ],
             ),
+          ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        bottom: 10,
+                        left: 5,
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.white,
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              child: Icon(
+                                Icons.perm_identity,
+                                color: Colors.white,
+                                size: 35.0,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "John",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                buildListTile("My Account", Icons.people, () {}),
+                buildListTile("My Wi-Fi Settings", Icons.wifi, () {}),
+                buildListTile("Upgrade my plan", Icons.upgrade, () {}),
+                buildListTile("Shop", Icons.shopping_basket, () {}),
+                buildListTile("Offers", Icons.local_offer, () {}),
+                buildListTile(
+                    "Notifications", Icons.notification_important, () {}),
+                buildListTile("FAQ's", Icons.perm_identity, () {}),
+                buildListTile("About us", Icons.people_alt, () {}),
+                buildListTile("Rate This App", Icons.star, () {}),
+                buildListTile("Logout", Icons.logout, () {}),
+                buildListTile("App version: 0.0.1", Icons.new_releases, () {}),
+              ],
+            ),
+          ),
+          bottomNavigationBar: BottomAppBar(
+            color: Colors.white,
+            child: Container(
+              height: 70.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.home_outlined,
+                          size: 30.0,
+                          color: Colors.red,
+                        ),
+                        onPressed: () {},
+                      ),
+                      Text(
+                        "Home",
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.support_agent,
+                          size: 30.0,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {},
+                      ),
+                      Text(
+                        "Support",
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.east_rounded,
+                          size: 30.0,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {},
+                      ),
+                      Text(
+                        "Refer & Earn",
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.list,
+                          size: 30.0,
+                          color: isClicked ? Colors.red : Colors.grey[600],
+                        ),
+                        onPressed: () {},
+                      ),
+                      Text(
+                        "Bill details",
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              AccountTabView(),
+              HomeTabView(),
+              SocialTabView(),
+            ],
           ),
         ),
       ),
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,1 +1,64 @@
+import 'package:flutter/material.dart';
 
+class AccountData extends StatelessWidget {
+  final String title, data;
+
+  const AccountData({
+    Key key,
+    this.title,
+    this.data,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 10.0, left: 10),
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      data,
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Container(
+                        height: 20,
+                        width: 20,
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(100),
+                          ),
+                        ),
+                        child: Icon(Icons.edit, size: 15, color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
+  }
+}
